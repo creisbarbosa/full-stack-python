e30631aff4264f2f284552441b2e8604820b4413
Cassio Reis <creisbarbosa@gmail.com>
2022-08-15 21:50:12 -0300

    tarefas finalizadas

--- modulo-13 - JavaScript II/js/script.js
+++ modulo-13 - JavaScript II/js/script.js
@@ -1,4 +1,4 @@
-function averageCalculation( notes ) {
+function averageCalculation(notes) {
     let sum = 0;
     for ( d = 0; d < notes.length; d++) {
         sum += notes [d];
@@ -7,34 +7,32 @@ function averageCalculation( notes ) {
     return average;
 }
 
-//escopo global
-
-function aproval( average ) {
-		//escopo da função
+function aproval(notes) {
+    let average = averageCalculation(notes);
     let condition = average >= 7 ? "Aproved" : "Not aproved";
     return "Average note: " + average + " - Result: " + condition;
 }
 
-//console.log()
-//console.log('Average: ' + averageCalculation([7, 7, 7, 7]))
-console.log(aproval(averageCalculation([7, 6, 8, 10])));
+function regressiveCountdown (number){
+    console.log(number);
+    let nextNumber = number - 1;
+    if (nextNumber > 0)
+        regressiveCountdown(nextNumber);
+}
 
-document.addEventListener('submit', function(evento){
-    evento.preventDefault();
-    let formulary = document.getElementById('formulario-01');
+document.addEventListener('submit', function(event){
+    event.preventDefault();
+    event.stopPropagation();
+    let formulary = document.getElementById('formulary-01');
     let data = new FormData(formulary);
-    let objetc = {};
+    let object = {};
     let notes = [];
-    for(let key of data.keys()) {
+    for(let key of data.keys()){
         object[key] = data.get(key);
         //add itens in array
-        notes.push(parseIntdata(key));
+        notes.push(parseInt(data.get(key)));
     }
-
+    console.log(object);
     console.log(notes);
-    console.log(objetc);
-    
-    text = aproval(notes)
-
-    document.getElementById('result').innerHTML = aproval(notes)
+    document.getElementById('result').innerHTML = aproval(notes);
 });
